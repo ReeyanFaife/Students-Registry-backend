765134f6be2fe7934f186e909503f79ab2313bb5
Reeyan Faife <reeyanfaife61@gmail.com>
2026-05-07 13:30:57 +0200

    fix db include path

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
 
-require_once __DIR__ . '/../db.php';
+require_once __DIR__ . '/backend/db.php';
 
 $method = $_SERVER['REQUEST_METHOD'];
 
